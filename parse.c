f7abd04e1a63bde6ccde2eeacf1337f4109ea6f8
skeet1 <karimkar999@gmail.com>
2022-07-06 08:59:02 +0100

    fix a seg fault when there is a command empty

--- parse.c
+++ parse.c
@@ -6,7 +6,7 @@
 /*   By: mkarim <mkarim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/02 11:47:50 by mkarim            #+#    #+#             */
-/*   Updated: 2022/07/06 08:14:08 by mkarim           ###   ########.fr       */
+/*   Updated: 2022/07/06 08:44:50 by mkarim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,8 @@ int	main(int argc, char **argv)
 			data.cmd_line = readline(PROMPT);
 			add_history(data.cmd_line);
 			data.cmd_line = ft_strtrim(data.cmd_line, " ");
-			ft_token(&data, data.cmd_line);
+			if (ft_strlen(data.cmd_line))
+				ft_token(&data, data.cmd_line);
 		}
 	}
 }
